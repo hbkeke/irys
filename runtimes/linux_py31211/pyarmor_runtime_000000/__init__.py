172a9b2a66fc88540e68294b41c9072c85da7e5d
phoenix0x1 <221748789+phoenix0x1@users.noreply.github.com>
2025-10-01 22:08:38 +0000

    Armour build 9648cd3d2554b569cc0633c239b542de76031a68

---
+++ runtimes/linux_py31211/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.1.9 (trial), 000000, 2025-10-01T22:07:44.865961
+from .pyarmor_runtime import __pyarmor__
---
+++ runtimes/macos_py31210/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.1.9 (trial), 000000, 2025-10-01T22:07:39.381239
+from .pyarmor_runtime import __pyarmor__
